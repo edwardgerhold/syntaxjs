6278df6f8675693337652d5586f5d29583d55389
edwardgerhold <edward.gerhold@googlemail.com>
2014-01-30 11:06:07 +0100

    fixed stringiterator by fixing ToPrimitive (which fixes more)

---
+++ test/direct/lpad-rpad.js
@@ -0,0 +1,20 @@
+let s = "cdefghijklmnopqrstuvwx";
+
+console.log(s.lpad(10, "ab"));
+console.log(s.lpad(11, "ab"));
+console.log(s.lpad(12, "ab"));
+console.log(s.lpad(13, "ab"));
+console.log(s.lpad(14, "ab"));
+
+console.log(s.lpad(10, "ab"));
+console.log(s.lpad(11, "ab"));
+console.log(s.lpad(12, "ab"));
+console.log(s.lpad(13, "ab"));
+console.log(s.lpad(14, "ab"));
+
+console.log(s.rpad(1, "yz"));
+console.log(s.rpad(2, "yz"));
+console.log(s.rpad(3, "yz"));
+console.log(s.rpad(4, "yz"));
+console.log(s.rpad(5, "yz"));
+
