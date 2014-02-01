23aae46318820dedae460aa099bf16eb4ac4e7bb
edwardgerhold <edward.gerhold@googlemail.com>
2014-02-01 02:21:57 +0100

    #include "in1.js"; will be replaced by the  file.

---
+++ tools/inlinefiles.js
@@ -0,0 +1,52 @@
+/* node inlinefile.js top-level.js [out-file.js] wird top-level.js rekursiv nach #include "name.js" absuchen und die Dateien einbinden */
+
+(function main () {
+    "use strict";
+    
+    function inlineFiles(filename) {
+        var def = makePromise();
+        fs.readFile(filename, "utf8", function (err, data) {
+            if (err) def.reject(err);
+            else def.resolve(data);
+        });
+        return def.promise;
+    }
+    
+    function transForm(input) {
+        return input.replace(include, function (all, filename) {
+            var content = fs.readFileSync(filename, "utf8");
+            content = transForm(content);
+            return content;
+        });
+    }
+    
+    function writeFile(toDrive, data) {
+        fs.writeFile(toDrive, data, "utf8", function (err) {
+            if (err) console.dir(err);
+
+            else console.log(toDrive + " successfully written.");
+        });
+        return data;
+    }
+    
+    function logOrWrite (data) {
+    var toDrive = process.argv[3];
+    if (typeof toDrive === "string") {
+            if (fromFile === toDrive) console.log("input "+fromFile+" and output "+toDrive+" are the same");
+            else writeFile(toDrive, data);    
+        } else {
+            console.log(data);
+        }
+        return data;
+    }
+    
+    var include = /(?:\#include "([^\"]+)";)/g;
+    var makePromise = require("./promise.js").makePromise;
+    var fs = require("fs");
+    var fromFile = process.argv[2];
+    if (!fs.existsSync(fromFile)) throw new TypeError(fromFile + " does not exist");
+    
+    var ausgabeP = inlineFiles(fromFile).then(transForm).then(logOrWrite);
+    console.log("A promise - Diese Zeile wird vor der Zeile ausgegeben, die wir soeben riefen. - the turn.");
+    
+}());
