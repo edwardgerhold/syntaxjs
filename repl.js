0cc4215a0c0e4e2a17ab5fedc6cb6cf3a69dbce8
edwardgerhold <edward.gerhold@googlemail.com>
2014-02-10 22:31:57 +0100

    forgot to test the repl - this will replace the shell module for sure

---
+++ repl.js
@@ -0,0 +1,36 @@
+//
+// This module starts a real REPL for syntax.js
+// If you start node syntax0.js a simple shell starts
+// The repl has been developed a lot longer
+// 
+var syntaxjs = require("./syntax0.js").syntaxjs;
+var repl = require("repl");
+var net = require("net");
+function evalWithSyntaxJs(cmd, context, filename, callback) {
+    var result = syntaxjs.toValue(cmd, true);
+    callback(null, result);
+}
+var port = +process.argv[2]
+if (!port) {
+// node repl.js starts the repl
+    repl.start({
+        prompt: "es6> ",
+        input: process.stdin,
+        output: process.stdout,
+        useColors: true,
+        eval: evalWithSyntaxJs
+    });
+} else {
+// node repl.js <portnum 5001> starts the server
+    net.createServer(function(socket) {
+	repl.start({
+    	    prompt: "es6:5001> ",
+            input: socket,
+	    output: socket,
+    	    useColors: true,
+            eval: evalWithSyntaxJs
+	}).on("exit", function () {
+	    socket.end();
+	});
+    }).listen(port);
+}
