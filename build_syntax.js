7b570be0d4d834cf60415a43142cdc666e3e5def
edwardgerhold <edward.gerhold@googlemail.com>
2014-02-26 13:49:31 +0100

    build_syntax calls tools/inlinefiles.js to include /lib in a file.

---
+++ build_syntax.js
@@ -0,0 +1,25 @@
+/*
+    
+    syntax.js
+    built with full_build_from_lib
+    
+*/
+//#include "lib/amd-prolly.js";
+//#include "lib/tables.js";
+//#include "lib/i18n.js";
+//#include "lib/semantics.js";
+//#include "lib/tokenizer.js";
+//#include "lib/crockfords-parser.js";
+//#include "lib/parser.js";
+//#include "lib/heap.js";
+//#include "lib/compiler.js";
+//#include "lib/jscodegen.js";
+//#include "lib/api.js";
+//#include "lib/runtime.js";
+//#include "lib/highlighter.js";
+//#include "lib/highlighter-app.js";
+//#include "lib/fswraps.js";
+//#include "lib/worker.js";
+//#include "lib/shell.js";
+//#include "lib/syntaxjs.js";
+//#include "lib/app.js";
