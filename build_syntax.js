3a8ca29ed3dc581bf6525f31cd2de2f1bc922b4c
edwardgerhold <edward.gerhold@googlemail.com>
2014-03-30 22:23:07 +0200

    improving build by splitting source and editing it afterwards in a good IDE instead of in a text editor like 2013

--- build_syntax.js
+++ build_syntax.js
@@ -7,17 +7,17 @@
 //#include "lib/amd-prolly.js";
 //#include "lib/tables.js";
 //#include "lib/i18n.js";
-//#include "lib/semantics.js";
+//#include "lib/parsenodes/semantics.js";
 /*#include "lib/unicode/identifier-module.js"; // disabled */
-//#include "lib/tokenizer.js";
-//#include "lib/earlyerrors.js";
-//#include "lib/parser.js";
+//#include "lib/parsenodes/tokenizer.js";
+//#include "lib/parsenodes/earlyerrors.js";
+//#include "lib/parsenodes/parser.js";
 //#include "lib/heap.js";
-//#include "lib/compiler.js";
-//#include "lib/arraycompiler.js";
-//#include "lib/cstcodegen.js"; 
+//#include "lib/compile/compiler.js";
+//#include "lib/compile/arraycompiler.js";
+//#include "lib/codegen/cstcodegen.js";
 //#include "lib/api.js";
-//#include "lib/runtime.js";
+//#include "lib/parsenodes/runtime.js";
 //#include "lib/highlighter/highlighter.js";
 //#include "lib/highlighter/highlighter-app.js";
 //#include "lib/fswraps.js";
